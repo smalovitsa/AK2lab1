b3b747fa3a0d274a2410e206c46353aab614e79e
smalovitsa <stas.malovitsa@gmail.com>
2021-12-23 01:25:31 -0800

    add some comments

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,19 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+// comment line 1
+// comment line 2
+// comment line 3
+// comment line 4
+// comment line 5
+// comment line 6
+// comment line 7
+// comment line 8
+// comment line 9
+// comment line 10
+// comment line 11
+// comment line 12
+// comment line 13
+// comment line 14
+// comment line 15
